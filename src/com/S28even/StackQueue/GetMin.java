1fc07aa8a60372beb97aa8d32c9ef2ec36cbd53d
S28even <wangjp92@mail.dlut.edu.cn>
2016-07-12 02:14:04 -0700

    jjj

--- src/com/S28even/StackQueue/GetMin.java
+++ src/com/S28even/StackQueue/GetMin.java
@@ -4,6 +4,6 @@ public class GetMin {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("adfadf");
+        System.out.println("adfadfvvfff");
     }
 }
