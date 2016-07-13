7947caa4ff784826bfb57e2ee8d0e9c63a006d21
wangjunpeng <wangjp92@mail.dlut.edu.cn>
2016-07-14 00:51:19 +0800

    July 13

--- src/com/S28even/StackQueue/ReverseByRecursion.java
+++ src/com/S28even/StackQueue/ReverseByRecursion.java
@@ -1,7 +1,30 @@
 package com.S28even.StackQueue;
+import java.util.Stack;
 
 /**
  * Created by wangjunp on 2016/7/13.
  */
 public class ReverseByRecursion {
+    private static int getLastElement(Stack<Integer> stack){
+        if(stack.isEmpty()){
+            throw new RuntimeException("this stack is empty");
+        }
+        int first = stack.pop();
+        if(stack.isEmpty())
+            return first;
+        else{
+            int last = getLastElement(stack);
+            stack.push(first);
+            return last;
+        }
+    }
+
+    public static void reverse(Stack<Integer> stack){
+        if(stack.isEmpty())
+            return;
+        int last = getLastElement(stack);
+        reverse(stack);
+        stack.push(last);
+    }
+
 }
